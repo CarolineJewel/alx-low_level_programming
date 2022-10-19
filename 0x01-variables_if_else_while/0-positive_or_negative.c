13929aa64c1896476b8567d1d150c3a08c06e547
Your Name <you@example.com>
2022-10-19 05:50:46 -0700

    clang

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,9 @@
 #include <time.h>
 
 /**
- * main - assign a random number to the variable n each time the program is executed
+ * main - assign a random number to the variable n each time it is executed
+ * if the number is greater than 0 is positive, if the number is 0: is zero
+ * if the number is less than 0: is negative, followed by a new line
  * return:0 - if positive print 0 else print 1
  */
 int main(void)
